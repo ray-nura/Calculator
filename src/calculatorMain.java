f36ba44e46ffcdb82b6aafc4b09258719e135482
ray-nura <raynura182@gmail.com>
2021-04-05 13:29:34 -0700

    changed line-11 add TRY

--- src/calculatorMain.java
+++ src/calculatorMain.java
@@ -8,13 +8,14 @@ public class calculatorMain {
 		char operator;
 		double answer=0.0;
 		
-		Scanner obj = new Scanner (System.in);
-		System.out.println("Please enter first number: ");
-		number1 = obj.nextDouble();
-		System.out.println("write Arithmatic operator + or - or / or * ");
-		operator = obj.next().charAt(0);
-		System.out.println("Please enter second number: ");
-		number2 = obj.nextDouble();
+		try (Scanner obj = new Scanner (System.in)) {
+			System.out.println("Please enter first number: ");
+			number1 = obj.nextDouble();
+			System.out.println("write Arithmatic operator + or - or / or * ");
+			operator = obj.next().charAt(0);
+			System.out.println("Please enter second number: ");
+			number2 = obj.nextDouble();
+		}
 		
 		switch (operator) {
 		case '+': answer = number1+number2;
